6aa42e8331d86a5f59b1be86b3eaa76a2160b123
Levi Gerrits <levigerrits@gmail.com>
2021-01-07 13:22:05 +0100

    a

--- html/Abbonement.php
+++ html/Abbonement.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,32 +17,33 @@
 <body>
     <div class="grid-container">
         <?=include "Components/header.php";?>
+
         <main>
             <h1>Selecteer je gewenste abbonement</h1>
         <a href="/html/Registreren.php">
-        <div class="Abbonement1">
-            
-            <img src="/img/placeholder.png" width="80" height="80" alt="Abbo1">
-            <h2>Beginner</h2>
-            <p>Met het beginner abbonement kun je gemakkelijk en goedkoop de nieuwste films en series van vorig jaar
-                zien!</p>
-        </div>
-    </a>
-    <a href="/html/Registreren.php">
-        <div class="Abbonement2">
-            
-            <img src="/img/placeholder.png" width="80" height="80" alt="Abbo2">
-            <h2>Standaard</h2>
-            <p>Met het standaard abbonement kun je alle films zien die niet meer in de bioscoop draaien en series een week voordat ze op tv komen!</p>
-        </div>
-    </a>
-    <a href="/html/Registreren.php">
-        <div class="Abbonement3">
-            
-            <img src="/img/placeholder.png" width="80" height="80" alt="Abbo3">
-            <h2>Professional </h2>
-            <p>Met het professional abbonement kun je alle films zien VOORDAT ze in de bioscoop draaien, voor series geld het zelfde als het standaard abbonement</p>
-        </div>
+            <div class="Abbonement1">
+                
+                <img src="/img/placeholder.png" width="80" height="80" alt="Abbo1">
+                <h2>Beginner</h2>
+                <p>Met het beginner abbonement kun je gemakkelijk en goedkoop de nieuwste films en series van vorig jaar
+                    zien!</p>
+            </div>
+        </a>
+        <a href="/html/Registreren.php">
+            <div class="Abbonement2">
+                
+                <img src="/img/placeholder.png" width="80" height="80" alt="Abbo2">
+                <h2>Standaard</h2>
+                <p>Met het standaard abbonement kun je alle films zien die niet meer in de bioscoop draaien en series een week voordat ze op tv komen!</p>
+            </div>
+        </a>
+        <a href="/html/Registreren.php">
+            <div class="Abbonement3">
+                
+                <img src="/img/placeholder.png" width="80" height="80" alt="Abbo3">
+                <h2>Professional </h2>
+                <p>Met het professional abbonement kun je alle films zien VOORDAT ze in de bioscoop draaien, voor series geld het zelfde als het standaard abbonement</p>
+            </div>
         </a>
 
     </main>
@@ -47,6 +51,5 @@
     <?=include "Components/footer.php";?>
     </div>
 </body>
-
 
 </html>
--- index.php
+++ index.php
@@ -16,18 +16,7 @@
 
 <body>
     <div class="grid-container">
-        <header>
-            <div class="dropdown">
-                <a class="active" href="/index.html">&#9776;</a>
-                <div class="dropdown-content">
-                    <a href="/html/Abbonement.html">Abbonementen</a>
-                    <a href="/html/FilmOverzicht.html">Filmoverzicht</a>
-                </div>
-            </div>
-            <a id="Profiel" href="/html/ProfielPagina.html">$Naam</a>
-        </header>
-
-
+        <?=include "html/Components/header.php";?>
 
         <div class="title">
             <h1>Fletnix</h1>
@@ -61,21 +50,8 @@
             </form>
         </div>
 
-        <footer>
-            <ul>
-                <li><a href="/html/OverOns.html">Over ons</a> </li>
-                <li><a href="/html/ProfielPagina.html">Account</a> </li>
-                <li><a href="#">veelgestelde vragen</a> </li>
-            </ul>
-            <p>Vragen? Bel 0800-123-4567 </p>
-            <p> Alle rechten voorbehouden door Fletnix&#169; inc. </p>
-            <p> Het kopiëren/distribueren van ons assortiment is ten strikste verboden en daders zullen strafrechtelijk
-                vervolgd worden.</p>
-        </footer>
+        <?=include "html/Components/footer.php";?>
     </div>
 </body>
-
-
-
 
 </html>
