8fc3cb02ca79a57e72567b89716d6eef0f4966e5
Levi Gerrits <levigerrits@gmail.com>
2021-01-12 21:16:25 +0100

    filmdetail query's

--- applicatie/public/html/Actions/haalFilmDetails.php
+++ applicatie/public/html/Actions/haalFilmDetails.php
@@ -3,9 +3,20 @@ require_once "/srv/webserver/applicatie/public/html/components/connection.php";
 
 function haalFilmDetails($movie_id){
     
-    $sql = "SELECT title, publication_year, genrename, description, duration
-        FROM films F INNER JOIN Regisseur R ON  F.movie_id = R.movie_id
-        INNER JOIN filmgenre FG ON F.movie_id = FG.movie_id
+    $sql = "SELECT title, publication_year, description, duration
+        FROM films
+        WHERE movie_id = :movie_id";
+
+    $query = getConn()->prepare($sql);
+    $query->execute([':movie_id' => $movie_id]);
+    $data = $query->fetchall();
+    return $data;
+}
+
+function haalFilmGenres($movie_id){
+    
+    $sql = "SELECT genrename
+        FROM filmgenre FG
         WHERE F.movie_id = :movie_id";
 
     $query = getConn()->prepare($sql);
@@ -14,10 +25,21 @@ function haalFilmDetails($movie_id){
     return $data;
 }
 
-function haalCast($movie_id){
+function haalFilmCast($movie_id){
     
     $sql = "SELECT voornaam, achternaam
         FROM Persoon P INNER JOIN Cast C ON P.persoon_id = C.persoon_id
+        WHERE movie_id = :movie_id";
+
+    $query = getConn()->prepare($sql);
+    $query->execute([':movie_id' => $movie_id]);
+    $data = $query->fetchall();
+    return $data;
+}
+
+function haalFilmRegisseur($movie_id){
+    $sql = "SELECT voornaam, achternaam
+        FROM Persoon P INNER JOIN Regisseur R ON P.persoon_id = R.persoon_id
         WHERE movie_id = :movie_id";
 
     $query = getConn()->prepare($sql);
