57aec3e1022022c146436c5f2de16d1005d3adc0
JorianBorst <jorianx@live.nl>
2021-01-12 14:51:13 +0100

    fixed haalTekstop

--- applicatie/public/html/Actions/haalTekstOp.php
+++ applicatie/public/html/Actions/haalTekstOp.php
@@ -3,16 +3,9 @@ include "/srv/webserver/applicatie/public/html/components/connection.php";
 
 function haalTekstOp($page_id, $content_id)
 {
-
     $query = "SELECT text FROM content WHERE pagina_id = $page_id AND content_id = $content_id";
     $stmt = getConn()->query($query);
     while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        print_r($row);
+        return ($row['text']);
     }
-    // $sql = "SELECT text FROM content WHERE pagina_id = :pagina_id AND content_id = :content_id";
-    // $query = getConn()->prepare($sql);
-    // $query->execute([':pagina_id' => $page_id,':content_id' =>$content_id]);
-    // $data = $query->fetchall();
-    // print_r($data);
 }
-
