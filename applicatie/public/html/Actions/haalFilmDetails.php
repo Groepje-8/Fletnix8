61ec3c00e29ae8ea3f6208c1d222c8f3617a10b0
Levi Gerrits <levigerrits@gmail.com>
2021-01-13 14:59:36 +0100

    filmdetails

--- applicatie/public/html/Actions/haalFilmDetails.php
+++ applicatie/public/html/Actions/haalFilmDetails.php
@@ -2,7 +2,7 @@
 require_once "/srv/webserver/applicatie/public/html/components/connection.php";
 
 // Query hulp-functies
-function executeQuery($sql){
+function executeMovieIdQuery($sql, $movie_id){
     $query = getConn()->prepare($sql);
     $query->execute([':movie_id' => $movie_id]);
     $data = $query->fetchall();
@@ -21,7 +21,7 @@ function haalFilmDetails($movie_id){
         FROM films
         WHERE movie_id = :movie_id";
 
-    $data = executeQuery($sql);
+    $data =  executeMovieIdQuery($sql, $movie_id);
     return $data[0];
 }
 
@@ -30,7 +30,7 @@ function haalFilmGenres($movie_id){
         FROM filmgenre FG
         WHERE movie_id = :movie_id";
 
-    $data = executeQuery($sql);
+    $data =  executeMovieIdQuery($sql, $movie_id);
     return formatArray($data);
 }
 
@@ -39,7 +39,7 @@ function haalFilmCast($movie_id){
         FROM Persoon P INNER JOIN Cast C ON P.persoon_id = C.persoon_id
         WHERE movie_id = :movie_id";
 
-    $data = executeQuery($sql);
+    $data =  executeMovieIdQuery($sql, $movie_id);
     return formatArray($data);
 }
 
@@ -48,7 +48,7 @@ function haalFilmRegisseurs($movie_id){
         FROM Persoon P INNER JOIN Regisseur R ON P.persoon_id = R.persoon_id
         WHERE movie_id = :movie_id";
 
-    $data = executeQuery($sql);   
+    $data =  executeMovieIdQuery($sql, $movie_id);   
     return formatArray($data);
 }
 
--- applicatie/public/html/FilmDetails.php
+++ applicatie/public/html/FilmDetails.php
@@ -4,11 +4,17 @@ include "Actions/haalFilmDetails.php";
 require_once "Components/header.php";
 maak_header("FilmDetails");
 
-$movie_id = 1; //$_GET['movie_id'];
-$filmData = haalFilmDetails($movie_id);
-$filmGenres = haalFilmGenres($movie_id);
-$filmRegisseurs = haalFilmRegisseurs($movie_id);
-$filmCast = haalFilmCast($movie_id);
+//if (!isset($_GET['movie_id'])){
+    //header("location: /FilmOverzicht.php");
+
+$movie_id = $_GET['movie_id'];
+
+$film = [
+    'Details' => haalFilmDetails($movie_id),
+    'Genres' => haalFilmGenres($movie_id),
+    'Regisseurs' => haalFilmRegisseurs($movie_id),
+    'Cast' => haalFilmCast($movie_id),
+];
 ?>
 <body>
     <?php require_once "Components/navbar.php";  ?>
@@ -21,18 +27,18 @@ $filmCast = haalFilmCast($movie_id);
             </form>
         </div>
         <div class="titel">
-            <h1><?=$filmData['title']?></h1>
-            <h2><?=$filmData['publication_year']?> - <?=formatDetailString($filmRegisseurs);?></h2>
+            <h1><?=$film['Details']['title']?></h1>
+            <h2><?=$film['Details']['publication_year']?> - <?=formatDetailString($film['Regisseurs']);?></h2>
             <a href="/html/FilmOverzicht.php">
                 <p>Meer van deze regisseur</p>
             </a>
-            <h3><?=formatDetailString($filmGenres);?></h3>
+            <h3><?=formatDetailString($film['Genres']);?></h3>
         </div>
         <div class="beschrijving">
 
             <h2>Samenvatting</h2>
-            <p><?=$filmData['description']?></p>
-            <p>Speelduur: <?=minutenNaarUur($filmData['duration'])?></p>
+            <p><?=$film['Details']['description']?></p>
+            <p>Speelduur: <?=minutenNaarUur($film['Details']['duration'])?></p>
         </div>
         <div class="poster">
             <img src="/img/Posters/<?=$movie_id?>.jpg" alt="Poster">
@@ -48,7 +54,7 @@ $filmCast = haalFilmCast($movie_id);
 
         </div>
         <div class="acteurgrid">
-            <?=printActeurGrid($filmCast)?>
+            <?=printActeurGrid($film['Cast'])?>
         </div>
     </main>
 
